23c697ee99dbb99cec888a1a3701fa27bbc940ee
Chrisp6349 <chrisparrish1@btinternet.com>
2026-07-26 20:38:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 // this shouldn't need a manual "clear site data" to take effect.
 // -----------------------------------------------------------------------
 
-const CACHE_VERSION = "v8";
+const CACHE_VERSION = "v9";
 const CACHE_NAME = `cadence-shell-${CACHE_VERSION}`;
 
 const SHELL_FILES = [
